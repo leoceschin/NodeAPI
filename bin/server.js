fdcd7f8d8c0c0bbb026cf0154d7ab4e8106ca1a3
Leonardo Francisco Ceschin <leoceschin@MacBook-Air-de-Leonardo.local>
2019-08-10 16:00:19 -0300

    Separando o servidor

--- bin/server.js
+++ bin/server.js
@@ -1,27 +1,17 @@
 'use strict'
 
+const app = require('../src/app');
 const http = require('http');
-const express = require('express');
 const debug = require('debug')('nodestr:server');
 
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
 
 console.log('Api rodando na porta ' + port);
 
@@ -63,4 +53,13 @@ function onError(error){
         default:
             throw error;
     }
+}
+
+//Iniciando o debug
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port '+ addr.port;
+    debug('Listening on ' + bind);
 }

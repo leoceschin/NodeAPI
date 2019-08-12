6f2a7b4ff4942e9042a50cfcddec18266fa4c2e6
Leonardo Francisco Ceschin <leoceschin@MacBook-Air-de-Leonardo.local>
2019-08-12 01:03:59 -0300

    Listando um Produto

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -3,6 +3,7 @@
 const router = require('express').Router();
 const productController = require('../controllers/product-controller');
 
+router.get('/', productController.get);
 router.post('/', productController.post);
 router.put('/:id', productController.put);
 router.delete('/', productController.delete);

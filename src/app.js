dfdc303a9e712d0e7ed74456708155aa557500ee
Leonardo Francisco Ceschin <leoceschin@MacBook-Air-de-Leonardo.local>
2019-08-12 00:49:01 -0300

    Criando um Produto

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const app = express();
 const router = express.Router();
 
 mongoose.connect('mongodb://leo:l123456@ds161517.mlab.com:61517/nodeapi', {useNewUrlParser: true});
+const Product = require('./models/product');
+
 
 //Carrega as rotas
 const indexRoute = require('./routes/index-route');
